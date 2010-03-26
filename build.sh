79dffbb73d692d8138d9be9da2d5be68870379d2
Paul Mucur <paul@beatthatquote.com>
2010-03-26 17:37:33 +0000

    Minify JSON2 as well.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SHA1=$(git show-ref --heads | awk '{print $1}')
-curl -s --data-urlencode js_code@js/st.js --data output_info=compiled_code --data compilation_level=SIMPLE_OPTIMIZATIONS -o js/stc.js http://closure-compiler.appspot.com/compile
+curl -s --data-urlencode js_code@js/st.js --data-urlencode js_code@js/json2.js --data output_info=compiled_code --data compilation_level=SIMPLE_OPTIMIZATIONS -o js/stc.js http://closure-compiler.appspot.com/compile
 sed -i"" "$ s/.*/# $SHA1/" spendthrift.manifest
 git add js/stc.js spendthrift.manifest
